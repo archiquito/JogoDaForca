87cbc672a577d7380eb80fb51a6a31510bab6980
archiquito <archiquito@gmail.com>
2018-03-19 21:41:56 -0300

    etapas 3 e 4

--- js/jogo.js
+++ js/jogo.js
@@ -1,34 +1,100 @@
-var criaJogo = function(){
+var criaJogo = function(sprite){
    
     var $palavra = $('#entrada').val();
+    var lacunas = [];
+    var etapa = 1;
+    var letra = '';
+    var ganhou = false;
+    var perdeu = false;
    
     var contaPalavra = $palavra.length;
     
     var arrPalavra = $palavra.split("");
     
-   
+
+    var criaLacunas = function(letra){   
+        
+        for(let i=0; i < palavraSecreta.length; i++){
+          
+            lacunas.push('');        
+  
+        }
+
+    };
+
+    var insereLetra = function(){
+
+    };
+
+    var proximaEtapa = function(num){
+        etapa = num;
+    };
+
     var setPalavraSecreta = function(palavra){
        
       palavraSecreta = palavra;
+      criaLacunas();
+      proximaEtapa(2);
      
        console.log(palavraSecreta);
     };
 
     var getLacunas = function(){
       
-        lacunas = setPalavraSecreta();
-        console.log(lacunas);
+       return lacunas;
     };
 
 
     var getEtapa = function(){
-        console.log()
+        return etapa;
+    };
+
+
+
+    var processaChute = function(chute){
+         var exp = new RegExp(chute, 'gi'),
+            resultado,
+            acertou = false;
+
+        while(resultado = exp.exec(palavraSecreta)){
+            lacunas[resultado.index] = chute;
+            acertou = true;
+        }
+
+        if(!acertou)sprite.nextFrame();
+
+    };
+
+    var ganhou = function(){
+        return (lacunas.length ? !lacunas.some(function(lacuna){ return lacuna == '';}) : false);
+
+    };
+
+    var perdeu = function(){
+       return sprite.finalizou();
+    };
+
+    
+    var ganhouOuPerdeu = function(){
+        return ganhou() || perdeu();
+    };
+
+    var reinicia = function(){
+        etapa = 1;
+        lacunas = [];
+        palavraSecreta ='';
+        sprite.reseta();
     };
 
     return{
         setPalavraSecreta: setPalavraSecreta,
         getLacunas: getLacunas,
-        getEtapa: getEtapa
+        getEtapa: getEtapa,
+        processaChute: processaChute,
+        ganhou: ganhou,
+        perdeu: perdeu,
+        ganhouOuPerdeu: ganhouOuPerdeu,
+        reinicia
     }
 
 };  
